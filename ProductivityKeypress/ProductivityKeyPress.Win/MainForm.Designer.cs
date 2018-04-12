1934a847080577e144b092dd2735a3f774c347e9
Andrei Lucaci <contact@andreilucaci.ro>
2018-04-12 15:41:27 +0300

    First commit

---
+++ ProductivityKeypress/ProductivityKeyPress.Win/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace ProductivityKeyPress.Win
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.keypressesTxtBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.averageSecTxtBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.averageMinTxtBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of KeyPresses: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this.keypressesTxtBox);
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Controls.Add(this.averageSecTxtBox);
+            this.flowLayoutPanel1.Controls.Add(this.label3);
+            this.flowLayoutPanel1.Controls.Add(this.averageMinTxtBox);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(460, 106);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // keypressesTxtBox
+            // 
+            this.keypressesTxtBox.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.keypressesTxtBox.Location = new System.Drawing.Point(197, 3);
+            this.keypressesTxtBox.Name = "keypressesTxtBox";
+            this.keypressesTxtBox.ReadOnly = true;
+            this.keypressesTxtBox.Size = new System.Drawing.Size(239, 27);
+            this.keypressesTxtBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Average keys / second:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // averageSecTxtBox
+            // 
+            this.averageSecTxtBox.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.averageSecTxtBox.Location = new System.Drawing.Point(197, 36);
+            this.averageSecTxtBox.Name = "averageSecTxtBox";
+            this.averageSecTxtBox.ReadOnly = true;
+            this.averageSecTxtBox.Size = new System.Drawing.Size(239, 27);
+            this.averageSecTxtBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(188, 30);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Average keys / minute:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // averageMinTxtBox
+            // 
+            this.averageMinTxtBox.Font = new System.Drawing.Font("Lucida Sans Unicode", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.averageMinTxtBox.Location = new System.Drawing.Point(197, 69);
+            this.averageMinTxtBox.Name = "averageMinTxtBox";
+            this.averageMinTxtBox.ReadOnly = true;
+            this.averageMinTxtBox.Size = new System.Drawing.Size(239, 27);
+            this.averageMinTxtBox.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(170, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 161);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 200);
+            this.MinimumSize = new System.Drawing.Size(500, 200);
+            this.Name = "MainForm";
+            this.Text = "Productivity KeyPress";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox keypressesTxtBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox averageSecTxtBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox averageMinTxtBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ProductivityKeypress/ProductivityKeyPress.Win/MainForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProductivityKeyPress.Win
+{
+    public partial class MainForm : Form
+    {
+        private volatile int _counter;
+        private Stopwatch _stopwatch;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            InitStopWatch();
+
+            SneakyListener.KeyPress += (sender, args) =>
+            {
+                _counter++;
+                Task.Run(() =>
+                {
+                    keypressesTxtBox.Invoke((Action)(() =>
+                   {
+                       keypressesTxtBox.Text = _counter.ToString();
+                   }));
+                    averageSecTxtBox.Invoke((Action)(() =>
+                   {
+                       averageSecTxtBox.Text = ComputeAverageSecKey().ToString(CultureInfo.InvariantCulture);
+                   }));
+                    averageMinTxtBox.Invoke((Action)(() =>
+                   {
+                       averageMinTxtBox.Text = ComputeAverageMinKey().ToString(CultureInfo.InvariantCulture);
+                   }));
+                });
+            };
+        }
+
+        private decimal ComputeAverageSecKey()
+        {
+            return _counter / (decimal)_stopwatch.Elapsed.TotalSeconds;
+        }
+
+        private decimal ComputeAverageMinKey()
+        {
+            return _counter / (decimal)_stopwatch.Elapsed.TotalMinutes;
+        }
+
+        private void InitStopWatch()
+        {
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            _counter = default(int);
+            InitStopWatch();
+
+            averageSecTxtBox.Clear();
+            keypressesTxtBox.Clear();
+            averageMinTxtBox.Clear();
+        }
+    }
+}
